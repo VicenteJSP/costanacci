9a1f4518e035667f11bace7f3e613287fd3b9343
Vicente Santiago <vicentejsp@outlook.com>
2019-11-28 15:56:07 -0600

    Implementando Fibonacci de forma simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'costanacci';
+  fibonacci = 1;
+
+  calculaFibonacci(valor: number): number {
+    console.log(valor);
+    let fibonacci = 1;
+    let ant = 0;
+    let aux = 0;
+    for (let i = 0; i < valor - 1; i++) {
+      aux = fibonacci;
+      fibonacci = ant + fibonacci;
+      ant = aux;
+    }
+    return fibonacci;
+  }
 }
